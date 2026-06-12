7bb7ba7873f9b5d0e1028936534f8488c210c58a
fediaiandrii <lezukokitagava@gmail.com>
2026-06-12 17:48:22 +0300

    State Update Batching in Practice

--- 08-how-react-works/starter-howreactworks/src/App.js
+++ 08-how-react-works/starter-howreactworks/src/App.js
@@ -50,8 +50,6 @@ function Tabbed({ content }) {
   );
 }
 
-console.log(<DifferentContent test={23} />);
-
 function Tab({ num, activeTab, onClick }) {
   return (
     <button
@@ -67,8 +65,26 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("Render");
+
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes(likes => likes + 1);
+  }
+
+  function handleTripleInc() {
+    setLikes(likes => likes + 1);
+    setLikes(likes => likes + 1);
+    setLikes(likes => likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -84,13 +100,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
