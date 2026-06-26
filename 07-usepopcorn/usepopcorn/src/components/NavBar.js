ba07c3a202e8c55f5212a1d399de6cd0967fa951
fediaiandrii <lezukokitagava@gmail.com>
2026-06-26 16:21:12 +0300

    Creating useKey

--- 07-usepopcorn/usepopcorn/src/components/NavBar.js
+++ 07-usepopcorn/usepopcorn/src/components/NavBar.js
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import { useKey } from "../useKey";
 
 export function NavBar({ children }) {
   return <nav className="nav-bar">{children}</nav>;
@@ -16,22 +17,11 @@ export function Logo() {
 export function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(
-    function () {
-      function callback(e) {
-        if (document.activeElement === inputEl.current) return;
-
-        if (e.code === "Enter") {
-          inputEl.current.focus();
-          setQuery("");
-        }
-      }
-
-      document.addEventListener("keydown", callback);
-      return () => document.addEventListener("keydown", callback);
-    },
-    [setQuery],
-  );
+  useKey("Enter", function () {
+    if (document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
 
   return (
     <input
