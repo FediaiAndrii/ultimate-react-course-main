3460d4685d395e8eca6110dadf0dd5cc0659b9a4
fediaiandrii <lezukokitagava@gmail.com>
2026-07-09 14:28:25 +0300

    Linking Between Routes With <Link /> and <NavLink />

--- 11-worldwise/worldwise/src/pages/PageNotFound.jsx
+++ 11-worldwise/worldwise/src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>NotFound</h1>
+    </div>
+  );
 }
 
 export default PageNotFound;
