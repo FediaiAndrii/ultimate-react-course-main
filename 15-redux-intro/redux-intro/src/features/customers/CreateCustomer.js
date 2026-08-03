ec9706a32493cbd87b493ac2fe7a109f5f162961
fediaiandrii <lezukokitagava@gmail.com>
2026-08-03 15:05:56 +0300

    Professional Redux File Structure: State Slices

--- 15-redux-intro/redux-intro/src/features/customers/CreateCustomer.js
+++ 15-redux-intro/redux-intro/src/features/customers/CreateCustomer.js
@@ -12,16 +12,13 @@ function Customer() {
       <div className="inputs">
         <div>
           <label>Customer full name</label>
-          <input
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-          />
+          <input value={fullName} onChange={e => setFullName(e.target.value)} />
         </div>
         <div>
           <label>National ID</label>
           <input
             value={nationalId}
-            onChange={(e) => setNationalId(e.target.value)}
+            onChange={e => setNationalId(e.target.value)}
           />
         </div>
         <button onClick={handleClick}>Create new customer</button>
