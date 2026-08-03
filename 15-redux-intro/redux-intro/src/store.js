598f21d989966fc42aa5336030afb8b983869076
fediaiandrii <lezukokitagava@gmail.com>
2026-08-03 12:21:34 +0300

    Working With Action Creators

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -6,6 +6,8 @@ const initialState = {
   loanPurpose: "",
 };
 
+// const ACCOUNT_DEPOSIT = "account/deposit";
+
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
@@ -36,16 +38,33 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
 
-store.dispatch({ type: "account/withdraw", payload: 200 });
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(requestLoan(1000, "Buy a car"));
+console.log(store.getState());
+
+store.dispatch(payLoan());
 console.log(store.getState());
