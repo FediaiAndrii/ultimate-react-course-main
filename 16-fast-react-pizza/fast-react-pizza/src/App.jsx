879763fe19cb40d4acca3016ccbac98f0fa0dba3
fediaiandrii <lezukokitagava@gmail.com>
2026-08-07 15:10:28 +0300

    Handling Errors With Error Elements

--- 16-fast-react-pizza/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
@@ -10,12 +11,15 @@ import AppLoayout from "./ui/AppLoayout";
 const router = createBrowserRouter([
   {
     element: <AppLoayout />,
+    errorElement: <Error />,
+
     children: [
       { path: "/", element: <Home /> },
       {
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
