b95481e37e8687bc9d5f32f19e68088ce3e8c760
fediaiandrii <lezukokitagava@gmail.com>
2026-08-07 13:08:43 +0300

    Building the App Layout

--- 16-fast-react-pizza/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/App.jsx
@@ -4,16 +4,22 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLoayout from "./ui/AppLoayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
   {
-    path: "/menu",
-    element: <Menu />,
+    element: <AppLoayout />,
+    childern: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderID", element: <Order /> },
+    ],
   },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderID", element: <Order /> },
 ]);
 
 function App() {
