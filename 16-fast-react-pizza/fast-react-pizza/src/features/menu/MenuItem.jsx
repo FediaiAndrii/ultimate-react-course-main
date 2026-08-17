c3c4dfbb624d377e29b83524f88a2c9ef146e45b
fediaiandrii <lezukokitagava@gmail.com>
2026-08-17 16:19:07 +0300

    Deleting Cart Items

--- 16-fast-react-pizza/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,12 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
+import DeleteItem from "../cart/DeleteItem";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQueantityById } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
 
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQueantityById(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newItem = {
@@ -41,7 +45,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
