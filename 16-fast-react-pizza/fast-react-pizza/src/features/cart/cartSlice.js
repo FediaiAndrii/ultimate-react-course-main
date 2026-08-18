a3b3246fddb059884ea3a630baffc606419b0db4
fediaiandrii <lezukokitagava@gmail.com>
2026-08-18 12:25:46 +0300

    Using the Cart for New Orders

--- 16-fast-react-pizza/fast-react-pizza/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/fast-react-pizza/src/features/cart/cartSlice.js
@@ -42,9 +42,9 @@ export const {
 
 export default cartSlice.reducer;
 
-export const getUsername = (state) => state.user.username;
-
 export const getCart = (state) => state.cart.cart;
+
+export const getUsername = (state) => state.user.username;
 
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
