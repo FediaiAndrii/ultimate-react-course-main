74acddcd3727bff451714cd063e426364ac71e03
fediaiandrii <lezukokitagava@gmail.com>
2026-09-09 14:57:22 +0300

    Connecting Supabase With Our React App

---
+++ 17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://iblhvovmlneeggwspspg.supabase.co";
+const supabaseKey = "sb_publishable_gK0PLD_8jY_3WnvTAVSKPA_rExM71i5";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
