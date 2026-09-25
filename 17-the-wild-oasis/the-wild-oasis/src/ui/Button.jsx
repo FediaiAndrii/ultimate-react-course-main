4192b0d1d48537d1814a07f38345244853152d42
fediaiandrii <lezukokitagava@gmail.com>
2026-09-25 13:45:51 +0300

    Confirming Cabin Deletions

--- 17-the-wild-oasis/the-wild-oasis/src/ui/Button.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Button.jsx
@@ -53,8 +53,8 @@ const Button = styled.button`
   border-radius: var(--border-radius-sm);
   box-shadow: var(--shadow-sm);
 
-  ${props => sizes[props.size || "medium"]}
-  ${props => variations[props.variation || "primary"]}
+  ${props => sizes[props.$size || "medium"]}
+  ${props => variations[props.$variation || "primary"]}
 `;
 
 export default Button;
