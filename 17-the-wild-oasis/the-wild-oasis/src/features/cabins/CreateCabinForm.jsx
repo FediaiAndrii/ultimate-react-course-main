3b3feb4c4f3f35f34472a98cf939223c8ccec6cc
fediaiandrii <lezukokitagava@gmail.com>
2026-09-25 11:34:34 +0300

    Converting the Modal to a Compound Component

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -131,7 +131,7 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
       <FormRow>
         {/* type is an HTML attribute! */}
         <Button
-          variation="secondary"
+          $variation="secondary"
           type="reset"
           onClick={() => onCloseModal?.()}
         >
